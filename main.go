fa490ae84f154e93db263ef6711f27f5ede19070
Kevin Burke <kev@inburke.com>
2017-04-09 17:38:57 -0700

    add http-only configuration

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"html/template"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"regexp"
@@ -101,6 +102,9 @@ type FileConfig struct {
 	// defaults to 7065.
 	Port *int `yaml:"port"`
 
+	// Set to true to listen for HTTP traffic (instead of TLS traffic)
+	HTTPOnly bool `yaml:"http_only"`
+
 	// For TLS configuration.
 	CertFile string `yaml:"cert_file"`
 	KeyFile  string `yaml:"key_file"`
@@ -131,7 +135,6 @@ func main() {
 	// secrets. See flash.go and crypto.go for examples.
 	_ = key
 
-	mux := NewServeMux()
 	if c.Port == nil {
 		port, ok := os.LookupEnv("PORT")
 		if ok {
@@ -144,25 +147,36 @@ func main() {
 			*c.Port = DefaultPort
 		}
 	}
-	if c.CertFile == "" {
-		c.CertFile = "cert.pem"
-	}
-	if _, err := os.Stat(c.CertFile); os.IsNotExist(err) {
-		logger.Error("Could not find a cert file; generate using 'make generate_cert'", "file", c.CertFile)
-		os.Exit(2)
-	}
-	if c.KeyFile == "" {
-		c.KeyFile = "key.pem"
-	}
-	if _, err := os.Stat(c.KeyFile); os.IsNotExist(err) {
-		logger.Error("Could not find a key file; generate using 'make generate_cert'", "file", c.KeyFile)
-		os.Exit(2)
-	}
+	mux := NewServeMux()
 	mux = handlers.UUID(mux)
 	mux = handlers.Server(mux, "go-html-boilerplate")
 	mux = handlers.Log(mux)
 	mux = handlers.Duration(mux)
-	logger.Info("Starting server", "port", *c.Port)
-	listenErr := http.ListenAndServeTLS("127.0.0.1:"+strconv.Itoa(*c.Port), c.CertFile, c.KeyFile, mux)
-	logger.Error("server shut down", "err", listenErr)
+	addr := ":" + strconv.Itoa(*c.Port)
+	if c.HTTPOnly {
+		ln, err := net.Listen("tcp", addr)
+		if err != nil {
+			logger.Error("Error listening", "addr", addr, "err", err)
+			os.Exit(2)
+		}
+		logger.Info("Started server", "port", *c.Port)
+		http.Serve(ln, mux)
+	} else {
+		if c.CertFile == "" {
+			c.CertFile = "cert.pem"
+		}
+		if _, err := os.Stat(c.CertFile); os.IsNotExist(err) {
+			logger.Error("Could not find a cert file; generate using 'make generate_cert'", "file", c.CertFile)
+			os.Exit(2)
+		}
+		if c.KeyFile == "" {
+			c.KeyFile = "key.pem"
+		}
+		if _, err := os.Stat(c.KeyFile); os.IsNotExist(err) {
+			logger.Error("Could not find a key file; generate using 'make generate_cert'", "file", c.KeyFile)
+			os.Exit(2)
+		}
+		listenErr := http.ListenAndServeTLS("127.0.0.1:"+strconv.Itoa(*c.Port), c.CertFile, c.KeyFile, mux)
+		logger.Error("server shut down", "err", listenErr)
+	}
 }
